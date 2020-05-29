ef20e3206b9fa1168abfa465708089b80e1660de
Bartlomiej Gladys <bartekgladys@gmail.com>
2020-05-29 21:32:27 +0200

    fix bug with image ordering.

--- data.py
+++ data.py
@@ -5,10 +5,12 @@ import os
 import glob
 import skimage.io as io
 import skimage.transform as trans
-from skimage.color import rgb2gray
+from skimage import img_as_ubyte
+from skimage.color import rgb2gray, gray2rgb
 from skimage.transform import resize
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import cv2
+from sklearn.model_selection import train_test_split
 
 Street = [0, 0, 255]
 Building = [255, 0, 0]
@@ -20,7 +22,7 @@ COLOR_DICT = np.array([Street, Building, Unlabelled])
 im_width = 512
 im_height = 512
 border = 1
-imagesNumber = 2
+imagesNumber = 100
 path_image = './data/berlin/image/'
 path_label = './data/berlin/label/'
 
@@ -33,27 +35,21 @@ def get_path(path_type, id):
 def get_data(): 
     X = np.zeros((imagesNumber, im_height, im_width, 1), dtype=np.float32)
     y = np.zeros((imagesNumber, im_height, im_width, 1), dtype=np.float32)
-    orig_X = np.zeros((imagesNumber, im_height, im_width, 3), dtype=np.float32)
-    orig_y = np.zeros((imagesNumber, im_height, im_width, 3), dtype=np.float32)
 
     for id in range(1, imagesNumber + 1):        
 
-        x_img_org = img_to_array(load_img(get_path(path_image, id)))        
-        x_img_org = resize(x_img_org, (im_height, im_width, 3), mode='constant', preserve_range=True)
-        x_img = rgb2gray(x_img_org)
+        x_img = img_to_array(load_img(get_path(path_image, id), color_mode="grayscale" ))        
         x_img = resize(x_img, (im_height, im_width, 1), mode='constant', preserve_range=True)
+
         mask_org = img_to_array(load_img(get_path(path_label, id)))
         mask_org[np.where((mask_org==[255,0, 0]).all(axis=2))] = [255,255,255]
-        mask_org = resize(mask_org, (im_height, im_width, 3), mode='constant', preserve_range=True)                
         mask = rgb2gray(mask_org)
         mask = resize(mask, (im_height, im_width, 1), mode='constant', preserve_range=True)                
 
         X[id-1] = x_img / 255
         y[id-1] = mask / 255
-        orig_X[id-1] = x_img_org
-        orig_y[id-1] = mask_org
 
-    return X, y, orig_X, orig_y
+    return X, y
 
 
 
@@ -63,12 +59,23 @@ def labelVisualize(num_class,color_dict,img):
     img_out = np.zeros(img.shape + (3,))
     for i in range(num_class):
         img_out[img == i,:] = color_dict[i]
-    return img_out 
+    return img_out
 
 
 
-def saveResult(save_path, npyfile, originals, ground_truth, flag_multi_class = False,num_class = 2):
+def saveResult(save_path, npyfile, originals, ground_truths, flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
-        img = labelVisualize(num_class,COLOR_DICT,item)         
-        merge = np.concatenate((img, originals[i], ground_truth[i]), axis=1 )
+        # img = labelVisualize(num_class,COLOR_DICT,item)   
+        # print(np.unique(img))      
+        img = item[:,:,0] if len(item.shape) == 3 else item
+        img = img_as_ubyte(img)
+
+        original = originals[i][:, :, 0] if len(originals[i].shape) == 3 else originals[i]
+        original = img_as_ubyte(original)
+
+        ground_truth = ground_truths[i][:, :, 0] if len(ground_truths[i].shape) == 3 else ground_truths[i]
+        ground_truth = img_as_ubyte(ground_truth)
+        
+
+        merge = np.concatenate((img, original, ground_truth), axis=1 )
         io.imsave(os.path.join(save_path,"%d_predict.png"%i), merge)
--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from sklearn.model_selection import train_test_split
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-X, y, orig_X, orig_Y = get_data()
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.5, random_state=2018)
+X, y = get_data()
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.1, random_state=2018)
 
 
 
 model = unet((im_height, im_width, 1))
-model_checkpoint = ModelCheckpoint('unet_berlin.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit(X_train, y_train,steps_per_epoch=300,epochs=1, callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint('unet_berlin2.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model.fit(X_train, y_train,steps_per_epoch=10,epochs=9, callbacks=[model_checkpoint])
 
-model.load_weights('unet_berlin.hdf5')
+model.load_weights('unet_berlin2.hdf5')
 results = model.predict(X_valid)
 
 
-saveResult("data/berlin/test", results, orig_X, orig_Y)
+saveResult("data/berlin/test", results, X_valid, y_valid)
